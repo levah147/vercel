275c9595ce19e4d4e96c08b1504880fa37ff56bf
Levah <emmanuelnater@gmail.com>
2025-02-19 14:56:38 +0100

    okay

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,5 @@
-#!/usr/bin/env bash
-echo "BUILD START"
-pip3 install -r requirements.txt
-python3.9 manage.py collectstatic --noinput --clear
-echo "BUILD END"
+# #!/usr/bin/env bash
+# echo "BUILD START"
+# pip3 install -r requirements.txt
+# python3.9 manage.py collectstatic --noinput --clear
+# echo "BUILD END"

2b8280e34796a12f40bff4f46eaca4d7d69e67f6
Levah <emmanuelnater@gmail.com>
2025-02-19 15:05:13 +0100

    okay

--- build_files.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "BUILD START"
-pip install setuptools
-pip install build_files.sh
-pip install -r requirements.txt
-python manage.py migrate
-python manage.py collectstatic --noinput --clear
-echo "BUILD END"
